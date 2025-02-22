ee44ebb18ced9e432056251e1021bf779df7b7bb
Peerzada-Zain-Nizami <qazizainulabaidin@gmail.com>
2025-02-23 01:19:02 +0500

    ..

--- routes/api.php
+++ routes/api.php
@@ -23,8 +23,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::get('tasks', [TaskController::class, 'index']);
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('tasks', [TaskController::class, 'index']);
     Route::post('tasks', [TaskController::class, 'store']);
     Route::get('tasks/{id}', [TaskController::class, 'show']);
     Route::put('tasks/{id}', [TaskController::class, 'update']);
